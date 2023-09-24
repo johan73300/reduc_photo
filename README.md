85d40713ddb574542f1301ed4495ff29a9ab49e5
Johan Mattis PEDROLI <joped2953@uit.no>
2023-09-24 22:26:06 +0200

    Ajout taille cote

--- README.md
+++ README.md
@@ -2,14 +2,41 @@
 
 Ce programme consiste à réduire la taille de toutes les images d'un dossier.  
 Cela crée un dossier \<dossier\>\_reduc\_\<pourcentage\> et copie toutes les photos réduites dans celui-ci.  
-Pour cela, le programme utilise la commande **convert** du package **ImageMagick**.
+Pour cela, le programme utilise la commande des commandes du package **Pillow**.
 
-
+## Package nécessaires
+Le programme utilise des fonction de Pillow.
+Vous pouvez l'installer avec **pip**.
 
 ## Commande
+
+### Réduction en fonction d'un pourcentage
 ```sh
 ./reduc_image.py -d <dossier> -p <pourcentage>
 ```
-Dossier : chemin relatif ou chemin absolu  
-Pourcentage : Taille de l'image * pourcentage / 100
+* Dossier : chemin relatif ou chemin absolu  
+* Pourcentage : Taille de l'image * pourcentage / 100
 
+### Réduction en fonction de la taille du plus grand côté :
+```sh
+./reduc_image.py -d <dossier> -m <plus_grand_cote>
+```
+* Dossier : chemin relatif ou chemin absolu  
+* plus_grand_cote : Taille du plus grand côté
+
+
+## Aide
+```sh
+usage: reduc_image.py [-h] [-d DIRECTORY] [-p PERCENTAGE | -m MAX_SIDE]
+
+Decrease size of picture of one directory
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DIRECTORY, --directory DIRECTORY
+                        name of directory
+  -p PERCENTAGE, --percentage PERCENTAGE
+                        percentage decrease
+  -m MAX_SIDE, --max_side MAX_SIDE
+                        Maximum side size
+```
--- reduc_image.py
+++ reduc_image.py
@@ -1,31 +1,29 @@
 #!/bin/python3
 
 import argparse
-import subprocess # just to call an arbitrary command e.g. 'ls'
+import subprocess
 import os
+from PIL import Image
 
-PERCENTAGE_DEFAULT = 50
+MAX_SIDE_DEFAULT = 1200
         
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Log and plot sensors from file, serial line or mqtt msg...")
+    parser = argparse.ArgumentParser(description="Decrease size of picture of one directory")
     parser.add_argument("-d", "--directory", type=str,
-                    help="name of directory") 
-    parser.add_argument("-p", "--percentage", type=int, default=PERCENTAGE_DEFAULT,
+                    help="name of directory")
+    group = parser.add_mutually_exclusive_group()                
+    group.add_argument("-p", "--percentage", type=int,
                     help="percentage decrease")
-
+    group.add_argument("-m", "--max_side", type=int, default=MAX_SIDE_DEFAULT,
+                    help="Maximum side size") 
 
     args = parser.parse_args()
-
-    # Check if directory is existing
-
 
 
     directory = args.directory
 
-    print("Réduction des photos du dossier ", directory)
-    print("Reduction de", args.percentage)
     try:
         os.chdir(directory)
     except:
@@ -33,20 +31,62 @@ if __name__ == "__main__":
         print("./reduc_image.py <directory name>")
         exit()
 
-    print("Dossier :", os.getcwd())
+
+    print("Réduction des photos du dossier ", directory)
+
+    if(args.percentage != None):
+        print("Reduction de", args.percentage)
+    else:
+        print("Nouvelle taille du côté max est", args.max_side)
+
     os.chdir("..")
-    print("Dossier :", os.getcwd())
 
 
     picture_list = os.listdir(directory)
     
     name = directory.split("/")[-1]
     if(name == ""):
-        print("Condition")
         name = directory.split("/")[-2]
 
-    name_new = name+"_reduc_"+str(args.percentage)
-    os.mkdir(name_new)
-    for elem in picture_list:
-        commande = "convert -resize " + str(args.percentage)  + "% '"  + name + "'/" + elem + " '" + name_new + "'/" + elem 
-        os.system(commande)
+    if(args.percentage != None):
+        name_new = name+"_reduc_"+str(args.percentage)
+        os.mkdir(name_new)
+        new_route = name_new + "/"
+
+        for elem in picture_list:
+            im = Image.open(name + "/" + elem)
+            width = im.size[0]
+            height = im.size[1]
+
+            new_size = (int(width*args.percentage/100), int(height*args.percentage/100)) 
+            new_image = im.resize(new_size)
+
+            print("loop 1.5")
+
+            route_picture = new_route + elem
+            new_image.save(route_picture)
+
+            im.close()
+
+    else:
+        name_new = name + "_size_" + str(args.max_side)
+        os.mkdir(name_new)
+
+        new_route = name_new + "/"
+
+        for elem in picture_list:
+            im = Image.open(name + "/" + elem)
+
+            maxi = max(im.size)
+            width = im.size[0]
+            height = im.size[1]
+
+            percent = args.max_side/maxi
+
+            new_size = (int(width*percent), int(height*percent))
+            new_image = im.resize(new_size)
+
+            route_picture = new_route + elem
+            new_image.save(route_picture)
+
+            im.close()
